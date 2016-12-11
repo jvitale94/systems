39eded1640d7c34da11e6675e0b11561f17550d5
Jake Vitale <jakev1994@gmail.com>
2016-12-11 14:16:01 -0500

    almost done

--- ALU.sv
+++ ALU.sv
@@ -5,7 +5,8 @@ As you are given it, this ALU only implements Add
 module ALU(input logic  [31:0] I1,
 	   input logic [31:0] I2,
 	   input logic [4:0] Selector,
-	   output logic [31:0] O
+	   output logic [31:0] O,
+	   output logic [0:0] zero
 	   );
 
    //need a buch of logics to compute everything the ALU does, then mux them to select the right one, using Selector
@@ -15,13 +16,39 @@ module ALU(input logic  [31:0] I1,
    assign not_ins = ~I1;
    assign nori_ins = (~I1)&(~I2);
 
-   logic [4:0] 	Rsbits;
+   //ROTATE
+   logic [31:0] rolv0, rolv1, rolv2, rolv3, rolv4, rolv5, rolv6, rolv7;
+   assign rolv0 = I2;
+   assign rolv1 = {I2[30:0], I2[31:31]};
+   assign rolv2 = {I2[29:0], I2[31:30]};
+   assign rolv3 = {I2[28:0], I2[31:29]};
+   assign rolv4 = {I2[27:0], I2[31:28]};
+   assign rolv5 = {I2[26:0], I2[31:27]};
+   assign rolv6 = {I2[25:0], I2[31:26]};
+   assign rolv7 = {I2[24:0], I2[31:25]};
 
-   assign Rsbits = I1 [4:0];
-   
+   logic [31:0] rorv0, rorv1, rorv2, rorv3, rorv4, rorv5, rorv6, rorv7;
+   assign rorv0 = I2;
+   assign rorv1 = {I2[0:0], I2[31:1]};
+   assign rorv2 = {I2[1:0], I2[31:2]};
+   assign rorv3 = {I2[2:0], I2[31:3]};
+   assign rorv4 = {I2[3:0], I2[31:4]};
+   assign rorv5 = {I2[4:0], I2[31:5]};
+   assign rorv6 = {I2[5:0], I2[31:6]};
+   assign rorv7 = {I2[6:0], I2[31:7]};
+
+   mux8to1B32 muxrolv(I1[2:2], I1[1:1], I1[0:0], rolv7, rolv6, rolv5, rolv4, rolv3, rolv2, rolv1, rolv0, rolv);
+   mux8to1B32 muxrorv(I1[2:2], I1[1:1], I1[0:0], rorv7, rorv6, rorv5, rorv4, rorv3, rorv2, rorv1, rorv0, rorv);
+
+   //Brach conditional
+   logic [31:0] rtminusrs;
+   assign rtminusrs = I2 + (~I1 + 1'b1);
+   assign zero = ~(rtminusrs[31:31]);
 
    always_comb
      case(Selector)
+       0:O=rolv;
+       1:O=rorv;
        2:O=not_ins;
        7:O=nori_ins;
        16:O=sum;
@@ -29,11 +56,6 @@ module ALU(input logic  [31:0] I1,
        default:O=sum;
      endcase // case (Selector)
 
-   
-
   // logic [31:0] m1, m2, m3;   
-  // assign O = sum;
-   
-   
 	
 endmodule
--- DataPath.sv
+++ DataPath.sv
@@ -8,6 +8,7 @@ module DataPath(clock, pcQ, instr, pcD, regWriteEnable);
    output logic [31:0] pcQ;
    output logic [31:0] pcD;
    output logic [0:0] regWriteEnable;
+   logic [0:0] 	memWrite, RegDst, ALUSrc, MemtoReg, Branch, JumpReg, JumpandLink, alu4, alu3, alu2, alu1, alu0;
    
    // The PC is just a register
    // for now, it is always enabled so it updates on every clock cycle
@@ -16,19 +17,32 @@ module DataPath(clock, pcQ, instr, pcD, regWriteEnable);
 
    //set up a hard-wired connection to a value
    logic [31:0] constant4;
+   
    initial
      constant4 <= 32'b100;
 
    // construct the adder for the PC incrementing circuit.
-   logic [31:0] adderIn1, adderIn2, adderOut, pcmux;
+   logic [31:0] adderIn1, adderIn2, adderOut, pcmux, jumpins, jumpout;
+   logic [31:0]        WD3, RD1, RD2;
    adder psAdd(adderIn1,adderIn2,adderOut);
-
+   logic [0:0] 	       zero;
+   logic [31:0]        PCBranch;
    //The mux before PC. Incomplete, but change when doing branch
-   mux4to1B32 PCprime(1'b0, 1'b0/*change this for branch*/, 32'b111111, 32'b101010, 32'b010101, adderOut, pcmux);
+   mux4to1B32 BranchorPlus4(1'b0, (Branch & zero), 32'b111111, 32'b101010, PCBranch, adderOut, pcmux);
+
+   mux4to1B32 JALorJR(JumpandLink, JumpReg, 32'b111111, jumpins, RD1/*contents of register 7*/, pcmux, jumpout);
    
+   assign jumpins = {adderOut[31:28], instr[25:0], {2{1'b0}}};
+
    assign adderIn1 = pcQ;
    assign adderIn2 = constant4;
-   assign pcD = pcmux;
+   assign pcD = jumpout;
+
+   always @ (negedge clock)
+     begin
+	$display ("PCD %b", pcD);
+     end
+   
 
    //Instruction Memory
    logic [31:0] instA;
@@ -37,9 +51,9 @@ module DataPath(clock, pcQ, instr, pcD, regWriteEnable);
 
    //CONTROL UNIT
    //Control outputs
-   logic [0:0] 	memWrite, RegDst, ALUSrc, MemtoReg, Branch, alu4, alu3, alu2, alu1, alu0;
+   //logic [0:0] 	memWrite, RegDst, ALUSrc, MemtoReg, Branch, JumpReg, JumpandLink, alu4, alu3, alu2, alu1, alu0;
    logic [4:0] 	ALUControl;
-   Control theControl(instr, memWrite, regWriteEnable, RegDst,ALUSrc, MemtoReg,ALUControl,Branch, alu4, alu3, alu2, alu1, alu0);
+   Control theControl(instr, memWrite, regWriteEnable, RegDst,ALUSrc, MemtoReg,ALUControl,Branch, JumpReg, JumpandLink, alu4, alu3, alu2, alu1, alu0);
 
    //Display Control outputs
    always @ (negedge clock)
@@ -49,20 +63,28 @@ module DataPath(clock, pcQ, instr, pcD, regWriteEnable);
 	$display ("ALUSrc %b", ALUSrc);
 	$display ("MemtoReg %b", MemtoReg);
 	$display ("ALUControl %b", ALUControl);
+	$display ("JumpReg %b", JumpReg);
+	$display ("JumpandLink %b", JumpandLink);
+	$display ("Branch %b", Branch);
+	$display ("zero %b", zero);
      end
    
    //Register File
-   logic [4:0] 	       A3, A2, A1, muxA3;
+   logic [4:0] 	       A3, A2, A1, muxA3, finalA3, finalA1;
    logic 	       WE3;
-   logic [31:0]        WD3, RD1, RD2;
+   //logic [31:0]        WD3, RD1, RD2;
    
    assign A2 = instr[20:16];
    assign clk = clock;
-   assign A1 = instr[25:21];	
+   mux2to1B5 muxforjr(JumpReg, 5'b00111, instr[25:21], finalA1);
+   
+   assign A1 = finalA1;	
    assign WE3 = regWriteEnable;
    //Change control when r-type is finished. It will be RegDst
    mux2to1B5 muxforA3(RegDst, instr[15:11], instr[20:16], muxA3);
-   assign A3 = muxA3;
+   mux2to1B5 muxA3final (JumpandLink, 5'b00111, muxA3, finalA3);
+   
+   assign A3 = finalA3;
 
    always @ (negedge clock)
      begin
@@ -80,25 +102,26 @@ module DataPath(clock, pcQ, instr, pcD, regWriteEnable);
   // logic [31:0]        BranchadderIn1, BranchadderIn2, PCBranch;
   // assign BranchadderIn1 = SignShift;
   // assign BranchadderIn2 = adderOut;
-   logic [31:0]        PCBranch;
+   //logic [31:0]        PCBranch;
    adder BrachAdd(SignShift,adderOut,PCBranch);
 
    always @ (negedge clock)
      begin
 	$display("SignImm   %b", SignImm);
 	$display("SignShift %b", SignShift);
-	$display("adderOut %b", adderOut);
-	$display("PCBranch %b", PCBranch);
+	$display("adderOut  %b", adderOut);
+	$display("PCBranch  %h", PCBranch);
      end
    
    //ALU
    logic [31:0]        SrcA, SrcB, ALUResult, ALUmuxOut;
    logic [4:0] 	       aluSelect;
+   //logic [0:0] 	       zero;
    assign SrcA = RD1;
    mux4to1B32 muxforALUSrcB(1'b0,ALUSrc, 32'b111111, 32'b101010, SignImm, RD2, ALUmuxOut);
    assign SrcB = ALUmuxOut;
    
-   ALU theALU(SrcA, SrcB, ALUControl, ALUResult);
+   ALU theALU(SrcA, SrcB, ALUControl, ALUResult, zero);
 
    always @ (negedge clock)
      begin
@@ -109,7 +132,7 @@ module DataPath(clock, pcQ, instr, pcD, regWriteEnable);
 
    //Data Memory
    logic [31:0]   RD;
-   logic [31:0]        WD,dataA, ALUorData;
+   logic [31:0]        WD,dataA, ALUorData, finalWD3;
    logic [0:0] 	       WE;
    assign WD = RD2;
    assign dataA = ALUResult;
@@ -118,7 +141,9 @@ module DataPath(clock, pcQ, instr, pcD, regWriteEnable);
 
    //What is read back to register file?
    mux4to1B32 muxALUorRD(1'b0, MemtoReg, 32'b111111, 32'b101010, RD, ALUResult, ALUorData);
-   assign WD3 = ALUorData;
+   mux4to1B32 muxfinalWD3(1'b0, JumpandLink, 32'b111111, 32'b101010, adderOut, ALUorData, finalWD3);
+   
+   assign WD3 = finalWD3;
    always @ (negedge clock)
      begin
 	$display("RD %b", RD);
